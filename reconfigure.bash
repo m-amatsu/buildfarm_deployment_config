95ea96c31de9d6e2d50f541ce809d7ddfd3f4bd7
root <amt@isp.co.jp>
2020-05-26 16:11:27 +0900

    see https://github.com/ros-infrastructure/buildfarm_deployment/issues/224

--- reconfigure.bash
+++ reconfigure.bash
@@ -10,8 +10,8 @@ function usage {
 }
 
 BUILDFARM_DEPLOYMENT_PATH=/root/buildfarm_deployment
-BUILDFARM_DEPLOYMENT_URL=https://github.com/ros-infrastructure/buildfarm_deployment.git
-BUILDFARM_DEPLOYMENT_BRANCH=master
+BUILDFARM_DEPLOYMENT_URL=https://github.com/tecnalia-advancedmanufacturing-robotics/buildfarm_deployment.git
+BUILDFARM_DEPLOYMENT_BRANCH=admin-jnlp-temp
 
 script_dir="$(dirname $0)"
 
